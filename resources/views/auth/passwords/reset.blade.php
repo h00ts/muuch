4f2e6499c34d66743293c888f7859b2d76354826
rusgs <rusfgs@gmail.com>
2017-07-11 12:27:55 -0500

    save

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Recuperar contraseña</div>
+                <div class="panel-heading panel-primary">Crea una nueva contraseña</div>
 
                 <div class="panel-body">
                     @if (session('status'))
