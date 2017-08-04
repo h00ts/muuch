c0eb4d1ad8655ab431d3524acac505c28a01a788
rusgs <rusfgs@gmail.com>
2017-08-04 12:18:33 -0500

    save

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -19,6 +19,7 @@
         </div>
             <div style="column-count: 2; column-gap: 25px; ">
             @foreach($categories->where('parent_id', null) as $category)
+                @permission('category-'.$category->slug)
                     <div class="panel panel-default" style="display: inline-block; margin: 10px auto; width: 100%;">
                         <div class="panel-body">
                             <div class="row">
@@ -27,15 +28,20 @@
                                 </div>
                                 <div class="col-md-8">
                                     @foreach($categories->where('parent_id', $category->id) as $subcategory)
+                                        @permission('category-'.$subcategory->slug)
                                         <a href="/muuch/cat/{{ $subcategory->id }}" class="btn btn-default btn-block" style="text-align:left"><i class="material-icons">folder_open</i> {{ $subcategory->name }}</a>
+                                        @endpermission
                                     @endforeach
                                     @foreach($category->pages as $page)
+                                        @permission('page-'.$page->slug)
                                         <a href="/muuch/{{ $page->id }}" class="btn btn-default btn-block" style="text-align:left"><i class="material-icons">chevron_right</i> {{ $page->name }}</a>
+                                        @endpermission
                                     @endforeach
                                 </div>
                             </div>
                         </div>
                     </div>
+                @endpermission
             @endforeach
             </div>
         </div>
